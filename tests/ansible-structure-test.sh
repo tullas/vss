32ce72e63d577092e733a3731bd9c27a89754d22
tullas <t.ullas@gmail.com>
2026-07-26 16:41:10 -0400

    Fix Ansible validation pipeline

--- tests/ansible-structure-test.sh
+++ tests/ansible-structure-test.sh
@@ -5,13 +5,14 @@ project_root=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)
 ansible_dir="$project_root/ansible"
 
 required_files=(
-  ansible.cfg
   playbooks/site.yml
   roles/baseline/defaults/main.yml
   roles/baseline/tasks/main.yml
   inventories/development/hosts.yml
-  group_vars/all/vault.yml
+  group_vars/all/vault.yml.example
 )
+
+[[ -f $project_root/ansible.cfg ]]
 
 for file in "${required_files[@]}"; do
   [[ -f $ansible_dir/$file ]]
